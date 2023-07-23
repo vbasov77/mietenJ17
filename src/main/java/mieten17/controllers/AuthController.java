6e07b3cb80ed0fd71210b7cebaaa7e3f08c4e57a
Vbasov <0120912@mail.ru>
2023-07-23 20:34:17 +0300

    перенесли сохранение нового юзера в userService

--- src/main/java/mieten17/controllers/AuthController.java
+++ src/main/java/mieten17/controllers/AuthController.java
@@ -4,23 +4,22 @@ package mieten17.controllers;
 import mieten17.models.Role;
 import mieten17.models.RoleUser;
 import mieten17.models.User;
-import mieten17.repositories.RoleRepository;
 import mieten17.repositories.UserRepository;
+import mieten17.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeSet;
 
 @Controller
 public class AuthController {
 
-    private RoleUser roleUser;
+    @Autowired
+    private UserService userService;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -49,12 +48,7 @@ public class AuthController {
             model.put("message", "Пользователь или email уже существуют!");
             return "auth/registration";
         }
-
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        TreeSet<Role> role = new TreeSet<>();
-        role.add(Role.valueOf("USER"));
-        user.setRoles(role);
-        userRepository.save(user);
+        userService.creatNewUser(user);
         return "redirect:/login";
     }
 
--- src/main/java/mieten17/services/UserService.java
+++ src/main/java/mieten17/services/UserService.java
@@ -1,21 +1,41 @@
 package mieten17.services;
 
+import mieten17.config.WebSecurityConfig;
+import mieten17.models.Role;
+import mieten17.models.User;
 import mieten17.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.TreeSet;
 
 @Service
 public class UserService implements UserDetailsService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return (UserDetails) userRepository.findByUsername(username);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void creatNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        TreeSet<Role> role = new TreeSet<>();
+        role.add(Role.valueOf("USER"));
+        user.setRoles(role);
+        save(user);
     }
 }
 
